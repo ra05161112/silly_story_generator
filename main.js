0506633ee221fda5e7f6ebaf246ee31ec7527c24
T.Miura <t.miura@cybermission.co.jp>
2021-07-16 14:42:55 +0900

    とりあえず完成.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function randomValueFromArray(array){
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-let insertx =["Willy the Goblin", "Big Daddy", "Father Christmas"];
+let insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
 let inserty = ["the soup kitchen", "Disneyland", "the White House"];
 
@@ -19,17 +19,24 @@ randomize.addEventListener('click', result);
 
 function result() {
 
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertx);
+  let yItem =randomValueFromArray(inserty) ;
+  let zItem = randomValueFromArray(insertz);
+
+  newStory = newStory.replace(":insertx:" , xItem).replace(":insertx:" , xItem).replace(":inserty:", yItem).replace(":insertz:", zItem);
+
   if(customName.value !== '') {
     let name = customName.value;
-
+    newStory = newStory.replace("Bob", name);
   }
 
   if(document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature =  Math.round(94);
-
+    let weight = Math.round(300*0.071429) + " stone";
+    let temperature =  Math.round((94-32)/1.8) + " centigrade";
+    newStory = newStory.replace("300 pounds", weight).replace("94 fahrenheit", temperature);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
